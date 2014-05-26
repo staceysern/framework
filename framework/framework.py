8f9b5fc198aa7f6555b1378ebb2fe1addd71b35a
Stacey Sern <staceysern@gmail.com>
2014-05-25 20:48:40 -0400

    Create wsgi application class

--- framework/framework.py
+++ framework/framework.py
@@ -1,33 +1,40 @@
 import os
 import webob
 
-BLOCK_SIZE = 4096
 
-def send_file(file_path, size):
-    with open(file_path) as f:
-        block = f.read(BLOCK_SIZE)
-        while block:
-            yield block
-            block = f.read(BLOCK_SIZE)
+class Framework(object):
+    BLOCK_SIZE = 4096
 
-def wsgiapp(environ, start_response):
-    request = webob.Request(environ)
+    def run(self, host='127.0.0.1', port=5000):
+        from waitress import serve
+        serve(self, host=host, port=port)
 
-    file_path = request.path[1:]
-    if file_path == '':
-        file_path = 'index.html'
+    def _send_file(self, file_path, size):
+        with open(file_path) as f:
+            block = f.read(self.BLOCK_SIZE)
+            while block:
+                yield block
+                block = f.read(self.BLOCK_SIZE)
 
-    if os.path.exists(file_path):
-        size = os.path.getsize(file_path)
-        headers = [('Content-type', 'text/html'),
-                   ('Content-length', str(size))]
+    def __call__(self, environ, start_response):
+        request = webob.Request(environ)
 
-        start_response('200 OK', headers)
-        return send_file(file_path, size)
-    else:
-        start_response('404 Not found', [('Content-type', 'text/plain')])
-        return [file_path + ' not found']
+        file_path = request.path[1:]
+        if file_path == '':
+            file_path = 'index.html'
+
+        if os.path.exists(file_path):
+            size = os.path.getsize(file_path)
+            headers = [('Content-type', 'text/html'),
+                       ('Content-length', str(size))]
+
+            start_response('200 OK', headers)
+            return self._send_file(file_path, size)
+        else:
+            start_response('404 Not found', [('Content-type', 'text/plain')])
+            return [file_path + ' not found']
+
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(wsgiapp, host='127.0.0.1', port=8080)
+    framework = Framework()
+    framework.run()
